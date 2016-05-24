624861ef6647c33e8b0f6b889ee0dc43096c1919
Johan Codinha <johan@codinha.net>
2016-05-24 23:53:57 +1000

    gulp initial setup

--- confeting/config/routes.rb
+++ confeting/config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get '/api/tickets/talk/:talk_id' => 'api/ticket#by_talk'
   # Tickets for a specific conf
   get '/api/tickets/conf/:conf_id' => 'api/ticket#by_conf'
+  # Tickets owned by an attendee
+  get '/api/tickets/attendee/:attendee_id' => 'api/ticket#by_attendee'
 end
